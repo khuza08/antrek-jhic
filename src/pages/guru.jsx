212bedcb5fb5ed1f52c966aec0393c27a0bb23a8
huza <huzaifahasif003@gmail.com>
2025-10-21 09:45:34 +0700

    test

--- src/pages/guru.jsx
+++ src/pages/guru.jsx
@@ -241,7 +241,7 @@ export default function Guru() {
                   onClick={applyFilters}
                   className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700"
                 >
-                  Apply
+                  Konfirmasi
                 </button>
               </div>
 
